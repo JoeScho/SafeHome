42a35c50ef8375051e2a65e85e480db0c462de78
JoeScho <joeschofield@live.co.uk>
2017-02-08 15:20:11 +0000

    some updates to various things ok thanks

--- SafeHome/SafeHome/Home.Designer.cs
+++ SafeHome/SafeHome/Home.Designer.cs
@@ -72,6 +72,12 @@
             this.pnlViewRooms = new System.Windows.Forms.Panel();
             this.btnAddRoom = new System.Windows.Forms.Button();
             this.btnLaunchEmulator = new System.Windows.Forms.Button();
+            this.listboxRooms = new System.Windows.Forms.ListBox();
+            this.lblRooms = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnlViewRoom = new System.Windows.Forms.Panel();
             this.pnlLogin.SuspendLayout();
             this.gbRegister.SuspendLayout();
             this.gbLogin.SuspendLayout();
@@ -295,7 +301,7 @@
             this.pnlAddRoom.Controls.Add(this.checkDoorN);
             this.pnlAddRoom.Controls.Add(this.comboRoomN);
             this.pnlAddRoom.Controls.Add(this.lblRoomName);
-            this.pnlAddRoom.Location = new System.Drawing.Point(18, 61);
+            this.pnlAddRoom.Location = new System.Drawing.Point(19, 285);
             this.pnlAddRoom.Name = "pnlAddRoom";
             this.pnlAddRoom.Size = new System.Drawing.Size(654, 218);
             this.pnlAddRoom.TabIndex = 8;
@@ -503,8 +509,13 @@
             // 
             // pnlViewRooms
             // 
+            this.pnlViewRooms.Controls.Add(this.label4);
+            this.pnlViewRooms.Controls.Add(this.textBox3);
+            this.pnlViewRooms.Controls.Add(this.label1);
+            this.pnlViewRooms.Controls.Add(this.lblRooms);
+            this.pnlViewRooms.Controls.Add(this.listboxRooms);
             this.pnlViewRooms.Controls.Add(this.btnAddRoom);
-            this.pnlViewRooms.Location = new System.Drawing.Point(18, 61);
+            this.pnlViewRooms.Location = new System.Drawing.Point(19, 61);
             this.pnlViewRooms.Name = "pnlViewRooms";
             this.pnlViewRooms.Size = new System.Drawing.Size(654, 218);
             this.pnlViewRooms.TabIndex = 7;
@@ -512,11 +523,11 @@
             // btnAddRoom
             // 
             this.btnAddRoom.Font = new System.Drawing.Font("American Typewriter", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddRoom.Location = new System.Drawing.Point(603, 15);
+            this.btnAddRoom.Location = new System.Drawing.Point(526, 57);
             this.btnAddRoom.Name = "btnAddRoom";
-            this.btnAddRoom.Size = new System.Drawing.Size(39, 39);
+            this.btnAddRoom.Size = new System.Drawing.Size(116, 39);
             this.btnAddRoom.TabIndex = 9;
-            this.btnAddRoom.Text = "+";
+            this.btnAddRoom.Text = "Add";
             this.btnAddRoom.UseVisualStyleBackColor = true;
             this.btnAddRoom.Click += new System.EventHandler(this.btnAddRoom_Click);
             // 
@@ -531,12 +542,67 @@
             this.btnLaunchEmulator.UseVisualStyleBackColor = true;
             this.btnLaunchEmulator.Click += new System.EventHandler(this.btnLaunchEmulator_Click);
             // 
+            // listboxRooms
+            // 
+            this.listboxRooms.Font = new System.Drawing.Font("Copperplate Gothic Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listboxRooms.FormattingEnabled = true;
+            this.listboxRooms.ItemHeight = 14;
+            this.listboxRooms.Location = new System.Drawing.Point(14, 28);
+            this.listboxRooms.Name = "listboxRooms";
+            this.listboxRooms.Size = new System.Drawing.Size(152, 144);
+            this.listboxRooms.TabIndex = 10;
+            // 
+            // lblRooms
+            // 
+            this.lblRooms.AutoSize = true;
+            this.lblRooms.Font = new System.Drawing.Font("Copperplate Gothic Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRooms.Location = new System.Drawing.Point(11, 12);
+            this.lblRooms.Name = "lblRooms";
+            this.lblRooms.Size = new System.Drawing.Size(55, 14);
+            this.lblRooms.TabIndex = 11;
+            this.lblRooms.Text = "Rooms";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Copperplate Gothic Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(523, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 14);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Add a new room";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(580, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(62, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Copperplate Gothic Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(523, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 14);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Floor";
+            // 
+            // pnlViewRoom
+            // 
+            this.pnlViewRoom.Location = new System.Drawing.Point(19, 509);
+            this.pnlViewRoom.Name = "pnlViewRoom";
+            this.pnlViewRoom.Size = new System.Drawing.Size(654, 218);
+            this.pnlViewRoom.TabIndex = 10;
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(684, 281);
+            this.ClientSize = new System.Drawing.Size(690, 741);
+            this.Controls.Add(this.pnlViewRoom);
             this.Controls.Add(this.btnLaunchEmulator);
             this.Controls.Add(this.pnlAddRoom);
             this.Controls.Add(this.pnlViewRooms);
@@ -555,6 +621,7 @@
             this.pnlAddRoom.ResumeLayout(false);
             this.pnlAddRoom.PerformLayout();
             this.pnlViewRooms.ResumeLayout(false);
+            this.pnlViewRooms.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -605,5 +672,11 @@
         private System.Windows.Forms.LinkLabel linkForgotPwd;
         private System.Windows.Forms.Label lblAddSensor;
         private System.Windows.Forms.Button btnLaunchEmulator;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRooms;
+        private System.Windows.Forms.ListBox listboxRooms;
+        private System.Windows.Forms.Panel pnlViewRoom;
     }
 }
